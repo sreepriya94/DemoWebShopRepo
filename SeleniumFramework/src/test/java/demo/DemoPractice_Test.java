9fd69ad875f0c59169cd95448034f71931a28493
sreep <sreep@SreepriyaLap>
2023-11-10 12:48:22 +0530

    this commit is after running command line

---
+++ SeleniumFramework/src/test/java/demo/DemoPractice_Test.java
@@ -0,0 +1,17 @@
+package demo;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+import genericUtilities.ITestListenerUtility;
+
+@Listeners(ITestListenerUtility.class)
+public class DemoPractice_Test extends BaseClass{
+	@Test
+	public void demoMethod()
+	{
+		System.out.println("done");
+	}
+
+}
